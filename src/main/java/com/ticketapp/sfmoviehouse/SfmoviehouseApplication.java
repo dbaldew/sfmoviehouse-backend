c8f0e060299bf2292cbbca32b2b4d750c8bfe435
dbaldew <dbaldew@gmail.com>
2022-09-03 17:26:21 +0200

    Initial commit

---
+++ src/main/java/com/ticketapp/sfmoviehouse/SfmoviehouseApplication.java
@@ -0,0 +1,13 @@
+package com.ticketapp.sfmoviehouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfmoviehouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfmoviehouseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ticketapp/sfmoviehouse/SfmoviehouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ticketapp.sfmoviehouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfmoviehouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
