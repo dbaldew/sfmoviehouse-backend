5e5556b8a2c3de8cb5262872ddf73cd78736fb09
dbaldew <dbaldew@gmail.com>
2022-10-07 13:16:43 +0200

    deleted event class

--- src/main/java/com/ticketapp/sfmoviehouse/repository/EventRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ticketapp.sfmoviehouse.repository;
-
-import com.ticketapp.sfmoviehouse.model.Event;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EventRepository extends CrudRepository<Event, Long> {
-}
