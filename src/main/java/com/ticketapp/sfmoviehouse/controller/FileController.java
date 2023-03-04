54cd2257beb8f0845b8819500034dd36e89a5ee6
dbaldew <dbaldew@gmail.com>
2023-03-04 15:18:29 +0100

    created File Exception handling & added credit line

--- src/main/java/com/ticketapp/sfmoviehouse/controller/FileController.java
+++ src/main/java/com/ticketapp/sfmoviehouse/controller/FileController.java
@@ -9,10 +9,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -20,7 +17,8 @@ import java.util.List;
 import java.util.stream.Collectors;
 //credit to Bezkoder at https://www.bezkoder.com/spring-boot-upload-file-database/
 
-@Controller
+@RestController
+@RequestMapping(value = "/files")
 public class FileController {
 
     @Autowired
@@ -30,20 +28,7 @@ public class FileController {
         this.fileService = fileService;
     }
 
-     @PostMapping("/upload")
-    public ResponseEntity<FileResponseMessage> uploadFile(@RequestParam("file")MultipartFile file){
-        String message = "";
-        try{
-            fileService.store(file);
-            message = "File upload success: " + file.getOriginalFilename();
-            return ResponseEntity.status(HttpStatus.OK).body(new FileResponseMessage(message));
-        }catch (Exception e){
-            message = "File upload failed: " + file.getOriginalFilename();
-            return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(new FileResponseMessage(message));
-        }
-     }
-
-     @GetMapping("files")
+     @GetMapping("")
     public ResponseEntity<List<ResponseFile>> getListOfFiles(){
         List<ResponseFile> files = fileService.getAllFiles().map(file -> {
             String fileDownloadUri = ServletUriComponentsBuilder
@@ -62,7 +47,7 @@ public class FileController {
         return ResponseEntity.status(HttpStatus.OK).body(files);
      }
 
-     @GetMapping("files/{id")
+     @GetMapping("id/{id}")
     public ResponseEntity<byte[]> getFile(@PathVariable Long id){
         File file = fileService.getFile(String.valueOf(id));
 
@@ -70,5 +55,18 @@ public class FileController {
                 .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename = \"" + file.getName() + "\"")
                 .body(file.getData());
      }
+
+    @PostMapping("")
+    public ResponseEntity<FileResponseMessage> uploadFile(@RequestParam("file")MultipartFile file){
+        String message = "";
+        try{
+            fileService.store(file);
+            message = "File upload success: " + file.getOriginalFilename();
+            return ResponseEntity.status(HttpStatus.OK).body(new FileResponseMessage(message));
+        }catch (Exception e){
+            message = "File upload failed: " + file.getOriginalFilename();
+            return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(new FileResponseMessage(message));
+        }
+    }
 
 }
