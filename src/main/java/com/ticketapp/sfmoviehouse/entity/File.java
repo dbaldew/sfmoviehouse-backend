1ff779ccc1f4f29782cdf14f5e970c5917afba3f
dbaldew <dbaldew@gmail.com>
2023-03-04 12:33:23 +0100

    created file.java

---
+++ src/main/java/com/ticketapp/sfmoviehouse/entity/File.java
@@ -0,0 +1,52 @@
+package com.ticketapp.sfmoviehouse.entity;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+
+public class File {
+
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name="uuid", strategy = "uuid2")
+    private String id;
+
+    private String name;
+
+    private String type;
+
+    @Lob
+    private byte[] data;
+
+    public File(String name, String type, byte[] data) {
+        this.name = name;
+        this.type = type;
+        this.data = data;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public byte[] getData() {
+        return data;
+    }
+
+    public void setData(byte[] data) {
+        this.data = data;
+    }
+}
