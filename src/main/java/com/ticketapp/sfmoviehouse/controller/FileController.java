99407220b81e5be63c046bd6f11a479c2787e7c8
dbaldew <dbaldew@gmail.com>
2023-03-08 23:40:09 +0100

    cleanup

--- src/main/java/com/ticketapp/sfmoviehouse/controller/FileController.java
+++ src/main/java/com/ticketapp/sfmoviehouse/controller/FileController.java
@@ -16,6 +16,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
+@CrossOrigin("http://localhost:8080")
 @RequestMapping (value = "/files")
 public class FileController {
 
