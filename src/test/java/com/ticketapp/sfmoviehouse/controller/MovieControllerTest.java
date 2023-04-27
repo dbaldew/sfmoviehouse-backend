f8f7cc5397b9bde3b24146966dfd99aae4804e43
dbaldew <dbaldew@gmail.com>
2023-04-27 16:58:23 +0200

    created MovieControllerTest & TicketControllerTest

---
+++ src/test/java/com/ticketapp/sfmoviehouse/controller/MovieControllerTest.java
@@ -0,0 +1,40 @@
+package com.ticketapp.sfmoviehouse.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MovieControllerTest {
+
+    @Test
+    void getAllMovies() {
+    }
+
+    @Test
+    void getAllMoviesByTitle() {
+    }
+
+    @Test
+    void getAllMoviesByYear() {
+    }
+
+    @Test
+    void getAllMoviesByCategory() {
+    }
+
+    @Test
+    void getMovieById() {
+    }
+
+    @Test
+    void addMovie() {
+    }
+
+    @Test
+    void updateMovie() {
+    }
+
+    @Test
+    void deleteMovie() {
+    }
+}
---
+++ src/test/java/com/ticketapp/sfmoviehouse/controller/TicketControllerTest.java
@@ -0,0 +1,36 @@
+package com.ticketapp.sfmoviehouse.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TicketControllerTest {
+
+    @Test
+    void getAllTickets() {
+    }
+
+    @Test
+    void getAllTicketsByUser() {
+    }
+
+    @Test
+    void getAllTicketsByTitle() {
+    }
+
+    @Test
+    void getTicketByID() {
+    }
+
+    @Test
+    void addTicket() {
+    }
+
+    @Test
+    void updateTicket() {
+    }
+
+    @Test
+    void deleteTicket() {
+    }
+}
