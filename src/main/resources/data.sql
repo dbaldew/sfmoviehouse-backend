ea35b5535c913a3728c3038e55520c0b5ed571f3
dbaldew <dbaldew@gmail.com>
2023-04-29 11:43:31 +0200

    setup MovieControllerTest

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -9,11 +9,11 @@ VALUES ('user', 'ROLE_USER'),
        ('admin', 'ROLE_ADMIN');
 
 INSERT INTO movies (category, description, summary, title, year)
-VALUES ('horror', 'holiday with sharks', 'Jaws', 'Jaws', 1979),
-       ('sf comedy', 'sf comedy', 'mars attacks', 'Mars Attacks', 1996),
-       ('future tech', 'artificial intelligence', 'blade runner', 'Blade Runner 2049', 2017),
-       ('fantasy', 'destroying a ring', 'first part of a journey to the land of Mordor', 'The Lord of the rings', 2001),
-       ('anime', 'dystopia', 'akira summary', 'Akira', 1988);
+VALUES ('horror', 'When a killer shark unleashes chaos on a beach community off Cape Cod, it''s up to a local sheriff, a marine biologist, and an old seafarer to hunt the beast down.', 'Jaws summary', 'Jaws', 1979),
+       ('sf comedy', 'Earth is invaded by Martians with unbeatable weapons and a cruel sense of humor.', 'Mars attacks summary', 'Mars Attacks', 1996),
+       ('future tech', 'Young Blade Runner K''s discovery of a long-buried secret leads him to track down former Blade Runner Rick Deckard, who''s been missing for thirty years.', 'Blade Runner summary', 'Blade Runner 2049', 2017),
+       ('fantasy', 'A meek Hobbit from the Shire and eight companions set out on a journey to destroy the powerful One Ring and save Middle-earth from the Dark Lord Sauron.', 'The Lord of the Rings: The Fellowship of the Ring summary', 'The Lord of the Rings: The Fellowship of the Ring', 2001),
+       ('anime', 'A secret military project endangers Neo-Tokyo when it turns a biker gang member into a rampaging psychic psychopath who can only be stopped by a teenager, his gang of biker friends and a group of psychics.', 'Akira summary', 'Akira', 1988);
 
 insert into tickets (cinema, date, time, movieid, username)
 VALUES (1, '12-12-2022', '20:00', 1, 'admin'),
--- src/test/java/com/ticketapp/sfmoviehouse/controller/MovieControllerTest.java
+++ src/test/java/com/ticketapp/sfmoviehouse/controller/MovieControllerTest.java
@@ -1,13 +1,87 @@
 package com.ticketapp.sfmoviehouse.controller;
 
+import com.ticketapp.sfmoviehouse.dto.MovieDTO;
+import com.ticketapp.sfmoviehouse.entity.Movie;
+import com.ticketapp.sfmoviehouse.service.MovieService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+@WebMvcTest(MovieController.class)
 class MovieControllerTest {
+
+    @MockBean
+    private MovieService movieService;
+    @Autowired
+    private MockMvc mockMvc;
+    @BeforeEach
+    public void setUp(){
+
+        Movie movie1 = new Movie(
+                1L,
+                "Jaws",
+                "1979",
+                "horror",
+                "Jaws",
+                "holiday When a killer shark unleashes chaos on a beach community off Cape Cod, it's up to a local sheriff, a marine biologist, and an old seafarer to hunt the beast down."
+        );
+
+        Movie movie2 = new Movie(
+                2L,
+                "Mars Attacks",
+                "1996",
+                "sf comedy",
+                "Mars attacks summary",
+                "Earth is invaded by Martians with unbeatable weapons and a cruel sense of humor."
+        );
+
+
+        MovieDTO movieDTO1 = new MovieDTO(
+                1L,
+                "Jaws",
+                "1979",
+                "horror",
+                "Jaws",
+                "holiday When a killer shark unleashes chaos on a beach community off Cape Cod, it's up to a local sheriff, a marine biologist, and an old seafarer to hunt the beast down."
+        );
+
+        MovieDTO movieDTO2 = new MovieDTO(
+                2L,
+                "Mars Attacks",
+                "1996",
+                "sf comedy",
+                "Mars attacks summary",
+                "Earth is invaded by Martians with unbeatable weapons and a cruel sense of humor."
+        );
+
+        MovieDTO toUpdateMovieDTO1 = new MovieDTO(
+                1L,
+                "Jaws",
+                "1979",
+                "horror",
+                "Jaws",
+                "holiday When a killer shark unleashes chaos on a beach community off Cape Cod, it's up to a local sheriff, a marine biologist, and an old seafarer to hunt the beast down."
+        );
+
+        MovieDTO toUpdateMovieDTO2 = new MovieDTO(
+                2L,
+                "Mars Attacks",
+                "1996",
+                "sf comedy",
+                "Mars attacks summary",
+                "Earth is invaded by Martians with unbeatable weapons and a cruel sense of humor."
+        );
+    }
 
     @Test
     void getAllMovies() {
+
+
+
     }
 
     @Test
